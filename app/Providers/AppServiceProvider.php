a8a41ed8fb719915ba91d89f349b53dbf70b3509
loic-cw <loic@chairwatch.com>
2026-06-02 08:16:07 -0400

    feat: disable SSL certificate verification for SMTP transport based on configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       
+         if (! config('mail.mailers.smtp.stream.ssl.verify_peer', true)) {
+            Mail::resolved(function ($mailer) {
+                $transport = $mailer->getSymfonyTransport();
+                if ($transport instanceof EsmtpTransport) {
+                    $transport->getStream()->setStreamOptions([
+                        'ssl' => [
+                            'verify_peer'      => false,
+                            'verify_peer_name' => false,
+                            'allow_self_signed' => true,
+                        ],
+                    ]);
+                }
+            });
+        }
     }
 }
